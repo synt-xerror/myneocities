f85f61fb899210ad3483c04ab5d364cdec74407c
SyntaxError! <me@stxerr.dev>
2026-09-21 10:14:36 -0300

    rename webrings to web-clubs, to include fan lists

--- tools/server.js
+++ tools/server.js
@@ -11,7 +11,7 @@ const port = 3001;
 
 app.use(morgan("dev"));
 
-const pages = ["/", "r", "about", "projects", "webrings", "neoshell", "git", "email", "articles", "pgp", "now", "minecraft_server"];
+const pages = ["/", "r", "about", "projects", "web-clubs", "neoshell", "git", "email", "articles", "pgp", "now", "minecraft_server"];
 
 for (const page of pages) {
   app.get(`/${page}`, (req, res) => {
